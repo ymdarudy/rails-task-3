e3d51d64213bb1eacfc2667410c619cf471571db
yuji yamada <ymdarudy@gmail.com>
2022-06-16 18:38:54 +0900

    add signin function

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :pictures
   has_many :favorites, dependent: :destroy
   has_many :favorite_pictures, through: :favorites, source: :pictures
+
+  mount_uploader :profile_image, ProfileImageUploader
+
+  has_secure_password
+  before_validation { email.downcase! }
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { minimum: 6 }
 end
