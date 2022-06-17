19743f84d03ae6da0b9ebdd4feda6fb1a754b97e
yuji yamada <ymdarudy@gmail.com>
2022-06-17 14:37:28 +0900

    feat: add image post function & access control

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,70 +1,55 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: %i[ show edit update destroy ]
+  before_action :ensure_user, only: %i[ edit update destroy ]
 
-  # GET /pictures or /pictures.json
   def index
-    @pictures = Picture.all
+    @pictures = Picture.all.order(updated_at: :desc)
   end
 
-  # GET /pictures/1 or /pictures/1.json
   def show
   end
 
-  # GET /pictures/new
   def new
     @picture = Picture.new
   end
 
-  # GET /pictures/1/edit
   def edit
   end
 
-  # POST /pictures or /pictures.json
   def create
-    @picture = Picture.new(picture_params)
-
-    respond_to do |format|
-      if @picture.save
-        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully created." }
-        format.json { render :show, status: :created, location: @picture }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    @picture = current_user.pictures.build(picture_params)
+    if @picture.save
+      redirect_to picture_url(@picture), notice: "新規投稿しました！"
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /pictures/1 or /pictures/1.json
   def update
-    respond_to do |format|
-      if @picture.update(picture_params)
-        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully updated." }
-        format.json { render :show, status: :ok, location: @picture }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    if @picture.update(picture_params)
+      redirect_to picture_url(@picture), notice: "投稿を編集しました！"
+    else
+      render :edit
     end
   end
 
-  # DELETE /pictures/1 or /pictures/1.json
   def destroy
     @picture.destroy
 
-    respond_to do |format|
-      format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to pictures_url, notice: "投稿を削除しました！"
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_picture
-      @picture = Picture.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def picture_params
-      params.require(:picture).permit(:content, :image, :user_id)
-    end
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
+
+  def ensure_user
+    redirect_to pictures_path unless @picture.user == current_user
+  end
+
+  def picture_params
+    params.require(:picture).permit(:content, :image, :image_cache, :user_id)
+  end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,4 @@
-class ProfileImageUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
@@ -30,7 +30,7 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  process resize_to_fill: [100, 100, "Center"]
+  process resize_to_fill: [300, 300, "Center"]
   # end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
