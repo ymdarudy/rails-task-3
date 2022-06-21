d6f29e910ff9c1ca960dbfd2f377035255af200f
yuji yamada <ymdarudy@gmail.com>
2022-06-21 15:22:59 +0900

    feat: no login access  control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
   before_action :set_user, only: %i[ show edit update ]
   before_action :ensure_user, only: %i[ edit update ]
 
